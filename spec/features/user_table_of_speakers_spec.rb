1e340a859c58d698a3a8bf00f346610f72ba0195
macheteshuffle <jeff.li029@gmail.com>
2014-01-22 20:36:40 -0500

    added to acceptance and unit tests and passed

--- spec/features/user_table_of_speakers_spec.rb
+++ spec/features/user_table_of_speakers_spec.rb
@@ -8,18 +8,14 @@ feature 'user sees table of roles', %q{
 } do
 
   scenario 'goes to the index page' do
-    file = File.open(Rails.root.join( "spec/test_parse/julius_caesar_test.xml"))
-    doc = Nokogiri::XML::Document.parse(file)
-    file.close
-
-    feed = XmlFeed.new(doc)
-    feed.feed_data
-
+    xml_parse
     visit root_path
     within("#myTable") do
       page.should have_content("FLAVIUS")
       page.should have_content("26")
       page.should have_content("10")
+      page.should have_content("1")
+      page.should have_content("6%")
     end
   end
 
--- spec/models/speaker_spec.rb
+++ spec/models/speaker_spec.rb
@@ -5,4 +5,31 @@ describe Speaker do
 
   it { should have_many :speeches }
   it { should have_many(:lines).through(:speeches) }
+  it { should have_many(:scenes).through(:speeches) }
+
+  it 'returns the longest speech for each speaker' do
+    xml_parse
+    speakers = Speaker.all
+    speeches = Speech.all
+    speakers.each do |speaker|
+      expect(speaker.longest_speech).to be_instance_of(Speech)
+    end
+  end
+
+  it 'returns the percentage each speaker shows in scenes' do
+    xml_parse
+    speakers = Speaker.all
+    speakers.each do |speaker|
+      expect(speaker.scene_percent).to be_instance_of(String)
+    end
+  end
+
+  it 'returns numbers of scenes each speaker appears in' do
+    xml_parse
+    scene_numbers = Speaker.scene_numbers
+    scene_numbers.each do |scene_number|
+      expect(scene_number).to be_instance_of(Hash)
+    end
+  end
+
 end
